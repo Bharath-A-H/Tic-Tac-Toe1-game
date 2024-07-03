9d04a1ee424b7cdabae43eb2c14a293a1f5d7a99
Bharath-A-H <156005611+Bharath-A-H@users.noreply.github.com>
2024-07-03 19:30:34 +0530

    Update GameResult.js

--- models/GameResult.js
+++ models/GameResult.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const gameResultSchema = new Schema({
-  winner: { type: String, required: true },
-  loser: { type: String, required: true },
-  gameState: { type: [String], required: true },
-  date: { type: Date, default: Date.now }
-});
+const gameResultSchema = new mongoose.Schema({
+  winner: {
+    type: String,
+    required: true,
+  },
+  loser: {
+    type: String,
+    required: true,
+  },
+  gameState: {
+    type: [String],
+    required: true,
+  },
+}, { timestamps: true });
 
-const GameResult = mongoose.model('GameResult', gameResultSchema);
-module.exports = GameResult;
+module.exports = mongoose.model('GameResult', gameResultSchema);
